f25ef2cdafae6388a6b2d55a46a483a0fba0f302
anstasiatum <anastasiya.tumakowa@gmail.com>
2025-07-07 18:39:10 +0300

    Improving hero creation

--- src/main/java/player/userinputhandler/commands/createnewhero/ChooseNobleOrKnight.java
+++ src/main/java/player/userinputhandler/commands/createnewhero/ChooseNobleOrKnight.java
@@ -4,30 +4,30 @@ import player.dndcharacter.DndCharacter;
 import player.userinputhandler.Response;
 import player.userinputhandler.State;
 
-import static player.userinputhandler.commands.createnewhero.Options.getEntertainerOrGladiatorOptions;
-import static player.userinputhandler.commands.createnewhero.OutputTexts.chooseMusicalInstrumentProficiency;
+import static player.userinputhandler.commands.createnewhero.Options.getKnightOrNobleOptions;
 import static player.userinputhandler.commands.createnewhero.OutputTexts.wrongInput;
 import static player.userinputhandler.enums.Processes.CREATE_HERO;
-import static player.userinputhandler.enums.Steps.CHOOSE_ENTERTAINER_OR_GLADIATOR;
-import static player.userinputhandler.enums.Steps.CHOOSE_MUSICAL_INSTRUMENT_YOU_ARE_PROFICIENT_WITH_FOR_ENTERTAINER;
-import static player.userinputhandler.enums.Steps.CHOOSE_WEAPON_FOR_GLADIATOR;
+import static player.userinputhandler.enums.Steps.CHOOSE_LANGUAGE_FOR_NOBLE;
+import static player.userinputhandler.enums.Steps.CHOOSE_NOBLE_OR_KNIGHT_FOR_NOBLE;
 
 public class ChooseNobleOrKnight {
     public static Response chooseNobleOrKnight(String userAnswer, DndCharacter dndCharacter) {
         Response response;
         State newState;
         response = switch (userAnswer.toLowerCase().trim()) {
-            case "entertainer":
-                dndCharacter.setFeaturesAndTraits(dndCharacter.getFeaturesAndTraits() + "You can perform at inns, theaters, circuses, or any place with a stage. While you’re performing there each night, you receive free modest or comfortable lodging and food. This can allow you to take long rests for free as you travel with your party across the land. In addition, your performance makes you famous wherever you perform. When strangers recognize you in the town, they usually like you more. This may make it easier to persuade them to do things for you.\n");
-                newState = new State(CREATE_HERO, CHOOSE_MUSICAL_INSTRUMENT_YOU_ARE_PROFICIENT_WITH_FOR_ENTERTAINER, dndCharacter);
-                yield new Response(newState, chooseMusicalInstrumentProficiency);
-            case "gladiator":
-                dndCharacter.setFeaturesAndTraits(dndCharacter.getFeaturesAndTraits() + "You can find a place to perform in any place that features combat for entertainment-perhaps a gladiatorial arena or secret pit fighting club.\n");
-                newState = new State(CREATE_HERO, CHOOSE_WEAPON_FOR_GLADIATOR, dndCharacter);
-                yield new Response(newState, "Enter an inexpensive, but unusual weapon (such as a trident or net) your hero will possess");
+            case "knight":
+                dndCharacter.setBackground(userAnswer);
+                dndCharacter.setFeaturesAndTraits(dndCharacter.getFeaturesAndTraits() + "You have the service of three retainers loyal to your family. These retainers can be attendants or messengers, and one might be a majordomo. Your retainers are commoners who can perform mundane tasks for you, but they do not fight for you, will not follow you into obviously dangerous areas (such as dungeons), and will leave if they are frequently endangered or abused.\n");
+                newState = new State(CREATE_HERO, CHOOSE_LANGUAGE_FOR_NOBLE, dndCharacter);
+                yield new Response(newState, "Choose a language your knight will know");
+            case "noble":
+                dndCharacter.setBackground(userAnswer);
+                dndCharacter.setFeaturesAndTraits(dndCharacter.getFeaturesAndTraits() + "You are welcome in high society, and people assume you have the right to be wherever you are. The common folk make every effort to accommodate you and avoid your displeasure, and other people of high birth treat you as a member of the same social sphere. You can secure an audience with a local noble if you need to.\n");
+                newState = new State(CREATE_HERO, CHOOSE_LANGUAGE_FOR_NOBLE, dndCharacter);
+                yield new Response(newState, "Choose a language your noble will know");
             default:
-                newState = new State(CREATE_HERO, CHOOSE_ENTERTAINER_OR_GLADIATOR, dndCharacter);
-                yield new Response(newState, wrongInput, getEntertainerOrGladiatorOptions());
+                newState = new State(CREATE_HERO, CHOOSE_NOBLE_OR_KNIGHT_FOR_NOBLE, dndCharacter);
+                yield new Response(newState, wrongInput, getKnightOrNobleOptions());
         };
         return response;
     }
